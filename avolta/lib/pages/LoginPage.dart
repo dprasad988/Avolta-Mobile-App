469aaef51726bd607f8e9461b8408a48c83a47de
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-09-21 22:28:22 +0530

    ..

--- avolta/lib/pages/LoginPage.dart
+++ avolta/lib/pages/LoginPage.dart
@@ -1,3 +1,4 @@
+import 'package:avolta/widgets/support_widgets.dart';
 import 'package:flutter/material.dart';
 
 class Loginpage extends StatefulWidget {
@@ -8,6 +9,8 @@ class Loginpage extends StatefulWidget {
 }
 
 class _LoginpageState extends State<Loginpage> {
+  bool _isObscured = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +18,172 @@ class _LoginpageState extends State<Loginpage> {
         child: Stack(
           alignment: Alignment.center,
           children: [
-             Positioned(
-                top: 108,
-                right: 50,
-                left: 50,
-                child: Image.asset(
-                  "images/logo.png",
-                  width: 273.0,
-                  height: 46.0,
-                  fit: BoxFit.cover,
-                ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 1.6,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)
+                    ),
+                    gradient: LinearGradient(colors: [
+                  Color.fromRGBO(60, 41, 165, 0.6),
+                  Color.fromRGBO(11, 8, 34, 0.4),
+                ], begin: Alignment.center, end: Alignment.bottomCenter,)),
               ),
+            ),
+            Positioned(
+              top: 108,
+              right: 50,
+              left: 50,
+              child: Image.asset(
+                "images/logo.png",
+                width: 273.0,
+                height: 46.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              child: Container(
+                  height: MediaQuery.of(context).size.height / 1.9,
+                  width: MediaQuery.of(context).size.width / 1.1,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25)),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.black),
+                        ),
+                      ),
+                      Center(
+                        child: Container(
+                          width: 200,
+                          child: Divider(
+                            thickness: 2,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("User Name", style: AppWidget.semiLabelStyle()),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: "Enter Your User Name",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(width: 2),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 10)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("Password", style: AppWidget.semiLabelStyle()),
+                      TextField(
+                          obscureText: _isObscured,
+                          decoration: InputDecoration(
+                            hintText: "Enter Your Password",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(width: 2),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 10),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _isObscured
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isObscured = !_isObscured;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: Text("Forgot Password ?", style: AppWidget.semiLabelStyle())
+                      ),
+                      SizedBox(height: 10,),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF8F53F0),
+                          borderRadius: BorderRadius.circular(5)
+                        ),
+                        child: Center(
+                          child: Text("Sign In", style: TextStyle(
+                            color: Colors.white, 
+                            fontWeight: FontWeight.bold),
+                          )
+                        )
+                      ),
+                      Center(
+                        child: Container(
+                          width: 260,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Divider(
+                                  thickness: 3,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text("Or Login in With"),
+                              ),
+                              Expanded(
+                                child: Divider(
+                                  thickness: 3,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 35,
+                                child: Image.asset(
+                                  "images/googleLogin.png",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Dont have an account ?"),
+                              Text(" Sign in" , style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),),
+                            ],
+                          ),
+                        ),
+                    ],
+                  )),
+            ),
           ],
         ),
       ),
---
+++ avolta/lib/pages/UserHomePage.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Userhomepage extends StatefulWidget {
+  const Userhomepage({super.key});
+
+  @override
+  State<Userhomepage> createState() => _UserhomepageState();
+}
+
+class _UserhomepageState extends State<Userhomepage> {
+  DateTime _focusedDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 180,
+            decoration: BoxDecoration(
+                color: Color(0xFFB9A1D6),
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(40))),
+            child: Padding(
+              padding: EdgeInsets.only(top: 30, left: 20, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Hi",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text("Dasun",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
+                      ClipOval(
+                        child: Image.asset(
+                          "images/user.png",
+                          fit: BoxFit.cover,
+                          height: 60,
+                          width: 60,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    "Your Shift Schedule",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 50,),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Container(
+              height: 400,
+              decoration: BoxDecoration(
+                color: Color(0xFFB9A1D6),
+                borderRadius: BorderRadius.circular(30)
+              ),
+              child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: TableCalendar(
+                focusedDay: _focusedDay,
+                firstDay: DateTime.utc(2020, 1, 1),
+                lastDay: DateTime.utc(2030, 1, 1),
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF8A4FFF),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  headerPadding: EdgeInsets.symmetric(vertical: 3),
+                  titleTextStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                  leftChevronIcon: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(5)
+                    ),
+                    child: Icon(
+                      Icons.chevron_left,
+                      color: Colors.white,
+                    ),
+                  ),
+                  rightChevronIcon: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(5)
+                    ),
+                    child: Icon(
+                      Icons.chevron_right,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  weekdayStyle: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  weekendStyle: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                  calendarStyle: CalendarStyle(
+                  defaultDecoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  weekendDecoration: BoxDecoration(
+                    color: Colors.white, 
+                  ),
+                ),
+              ),
+            )
+            ),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
