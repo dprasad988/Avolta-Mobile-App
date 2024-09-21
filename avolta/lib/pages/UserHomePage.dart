83b0ebd8c9ff5d451b2b8cea9efe15a22dfc3b38
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-09-22 00:27:30 +0530

    ..

--- avolta/lib/pages/UserHomePage.dart
+++ avolta/lib/pages/UserHomePage.dart
@@ -11,6 +11,7 @@ class Userhomepage extends StatefulWidget {
 class _UserhomepageState extends State<Userhomepage> {
   DateTime _focusedDay = DateTime.now();
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +20,9 @@ class _UserhomepageState extends State<Userhomepage> {
           Container(
             height: 180,
             decoration: BoxDecoration(
-                color: Color(0xFFB9A1D6),
-                borderRadius:
-                    BorderRadius.only(bottomLeft: Radius.circular(40))),
+              color: Color(0xFFB9A1D6),
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40)),
+            ),
             child: Padding(
               padding: EdgeInsets.only(top: 30, left: 20, right: 20),
               child: Column(
@@ -55,15 +56,13 @@ class _UserhomepageState extends State<Userhomepage> {
                         child: Image.asset(
                           "images/user.png",
                           fit: BoxFit.cover,
-                          height: 60,
-                          width: 60,
+                          height: 70,
+                          width: 70,
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: 25,
-                  ),
+                  SizedBox(height: 25),
                   Text(
                     "Your Shift Schedule",
                     style: TextStyle(
@@ -75,81 +74,102 @@ class _UserhomepageState extends State<Userhomepage> {
               ),
             ),
           ),
-          SizedBox(height: 50,),
+          SizedBox(height: 20),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 10),
             child: Container(
-              height: 400,
+              height: 480,
               decoration: BoxDecoration(
                 color: Color(0xFFB9A1D6),
-                borderRadius: BorderRadius.circular(30)
+                borderRadius: BorderRadius.circular(30),
               ),
-              child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: TableCalendar(
-                focusedDay: _focusedDay,
-                firstDay: DateTime.utc(2020, 1, 1),
-                lastDay: DateTime.utc(2030, 1, 1),
-                headerStyle: HeaderStyle(
-                  formatButtonVisible: false,
-                  titleCentered: true,
-                  decoration: BoxDecoration(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                      child: TableCalendar(
+                        focusedDay: _focusedDay,
+                        firstDay: DateTime.utc(2020, 1, 1),
+                        lastDay: DateTime.utc(2030, 1, 1),
+                        headerStyle: HeaderStyle(
+                          formatButtonVisible: false,
+                          titleCentered: true,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF8A4FFF),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          headerPadding: EdgeInsets.symmetric(vertical: 3),
+                          titleTextStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          leftChevronIcon: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 2),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Icon(
+                              Icons.chevron_left,
+                              color: Colors.white,
+                            ),
+                          ),
+                          rightChevronIcon: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 2),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Icon(
+                              Icons.chevron_right,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        daysOfWeekStyle: DaysOfWeekStyle(
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          weekdayStyle: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          weekendStyle: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        calendarStyle: CalendarStyle(
+                          defaultDecoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.rectangle
+                          ),
+                          weekendDecoration: BoxDecoration(
+                            color: Colors.white,
+                          ),
+                          todayDecoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    decoration: BoxDecoration(
                     color: Color(0xFF8A4FFF),
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  headerPadding: EdgeInsets.symmetric(vertical: 3),
-                  titleTextStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                  leftChevronIcon: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(5)
-                    ),
-                    child: Icon(
-                      Icons.chevron_left,
-                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
                     ),
                   ),
-                  rightChevronIcon: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(5)
-                    ),
-                    child: Icon(
-                      Icons.chevron_right,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                daysOfWeekStyle: DaysOfWeekStyle(
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  weekdayStyle: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  weekendStyle: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                  calendarStyle: CalendarStyle(
-                  defaultDecoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  weekendDecoration: BoxDecoration(
-                    color: Colors.white, 
-                  ),
-                ),
+                ],
               ),
-            )
             ),
           ),
-          
         ],
       ),
     );
